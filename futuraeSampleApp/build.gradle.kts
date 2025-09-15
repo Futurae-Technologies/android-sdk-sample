43e95d822d4204542db4ff442e08e4b5dbb95232
Nasia Koutsopoulou <nasia@futurae.com>
2025-09-15 16:27:05 +0300

    Update for v3.8.0-beta

--- futuraeSampleApp/build.gradle.kts
+++ futuraeSampleApp/build.gradle.kts
@@ -19,7 +19,7 @@ val getCommitCount: () -> Int = {
     stdout.toString().trim().toInt()
 }
 
-val sdkVersionName = "3.7.3-rc2"
+val sdkVersionName = "3.8.0-beta"
 
 android {
     namespace = "com.futurae.sampleapp"
