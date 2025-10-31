c51e98627213158530dee150a3ed9aeccbad879b
Nasia Koutsopoulou <nasia@futurae.com>
2025-10-31 15:20:10 +0200

    Expose flow to allow testing migration from corrupted v1 to v3

--- futuraeSampleApp/src/main/java/com/futurae/sampleapp/configuration/InitialConfigurationScreenRoute.kt
+++ futuraeSampleApp/src/main/java/com/futurae/sampleapp/configuration/InitialConfigurationScreenRoute.kt
@@ -6,10 +6,12 @@ import com.futurae.sampleapp.ui.shared.elements.snackbar.FuturaeSnackbarUIState
 @Composable
 fun InitialConfigurationScreenRoute(
     onConfigurationComplete: () -> Unit,
+    navigateToRecovery: () -> Unit,
     showSnackbar : suspend (FuturaeSnackbarUIState) -> Unit,
 ) {
     ConfigurationScreenRoute(
         onConfigurationComplete = onConfigurationComplete,
+        navigateToRecovery = navigateToRecovery,
         isConfigurationChange = false,
         pinProviderViewModel = null,
         onPinRequested = { },
