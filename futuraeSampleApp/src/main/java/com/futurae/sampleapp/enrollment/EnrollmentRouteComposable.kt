96601c670d0a102ad2213885d14a3043f09dd916
Nasia Koutsopoulou <nasia@futurae.com>
2026-01-07 11:31:14 +0200

    Disable Keyboard caching

--- futuraeSampleApp/src/main/java/com/futurae/sampleapp/enrollment/EnrollmentRouteComposable.kt
+++ futuraeSampleApp/src/main/java/com/futurae/sampleapp/enrollment/EnrollmentRouteComposable.kt
@@ -23,6 +23,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -195,8 +197,11 @@ fun FlowBindingTokenComposable(
                     focusedContainerColor = Tertiary,
                     unfocusedContainerColor = Tertiary
                 ),
+                visualTransformation = VisualTransformation.None,
                 keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Done
+                    imeAction = ImeAction.Done,
+                    autoCorrectEnabled = false,
+                    keyboardType = KeyboardType.Password
                 ),
                 keyboardActions = KeyboardActions(
                     onDone = {
